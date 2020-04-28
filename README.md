a86915879ef525a9462c46c7efb1c63c1b1dbc8b
Nguyễn Đức Anh <ducanhvna@outlook.com>
2020-04-28 11:00:38 +0700

    Update design inline
    Signed-off-by: Nguyễn Đức Anh <ducanhvna@outlook.com>

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 ## Installation
 
 ## Ref
+[Reading and Writing XML Files in Python](https://stackabuse.com/reading-and-writing-xml-files-in-python/)
+[](https://docs.python.org/3/library/xml.etree.elementtree.html#xml-tree-and-elements)
---
+++ characterlevel.py
@@ -0,0 +1,12 @@
+# character level 
+# Nhiem vu cua module laf chuyen tu dinh dang output cua doc creator sang character level ma tool khac cos the doc duoc Lableimg
+# Output foler: OuputCharacterLevel
+
+# Read all folder
+# Each subfoder
+#   
+#   Read groud truth
+#   Read OD
+#   Write to xml data
+#   Write to file to Output foder
+#   Copy image to output folder

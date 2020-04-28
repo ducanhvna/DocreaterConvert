bfdc7c417b3b8f4b56fcf9ca1d365f155dcd9f1c
Nguyễn Đức Anh <ducanhvna@outlook.com>
2020-04-28 11:46:57 +0700

    update output image
    Signed-off-by: Nguyễn Đức Anh <ducanhvna@outlook.com>

--- characterlevel.py
+++ characterlevel.py
@@ -14,7 +14,18 @@
 
 # Read all foder
 import os
+
+def ExtractData(folderpath, outLoction):
+    
+    return 1
+Extract = lambda i,o : ExtractData(i,o)
+
 directory = 'SampleData'
-os.walk(directory)
-listfolder = [x[0] for x in os.walk(directory)]
+outputfoder = 'OuputCharacterLevel'
+# Create Output foder if not exsts
+
+if not os.path.exists(outputfoder):
+    os.makedirs(outputfoder)
+
+listfolder = [Extract(x[0], outputfoder) for x in os.walk(directory)]
 print (listfolder)
